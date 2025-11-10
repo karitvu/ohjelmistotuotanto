41bead9d936916fff592b5b9516b31ba57bce3a6
karitvu <topi.val.kari@student.jyu.fi>
2025-11-10 20:33:57 +0200

    Tehtävän 8 tulostus tehty

---
+++ osa2/project-reader/src/project_reader.py
@@ -0,0 +1,22 @@
+from urllib import request
+from project import Project
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        #print(content)
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella 
+        testi = toml.loads(content)
+        nimi = testi['tool']['poetry']['name']
+        des = testi['tool']['poetry']['description']
+        dep = list(testi['tool']['poetry']['dependencies'].keys())
+        devdep = list(testi['tool']['poetry']['group']['dev']['dependencies'].keys())
+
+        return Project(nimi, des, dep, devdep)
